ad04d031af81c41bf301df075b956bafde77b1eb
Daniel Riverol <34141421+DanielRiverol@users.noreply.github.com>
2022-11-17 12:15:34 -0300

    Update main.js

--- clase_1/js/main.js
+++ clase_1/js/main.js
@@ -1,4 +1,6 @@
-//Variable los nombres deben descriptivos 
+//Variable los nombres deben ser descriptivos 
+
+//error lens theme => ONE DARK PRO
 
 //declaracion de variables
 var nombre; //ES5 no utilizada a partir de ES6
@@ -12,32 +14,33 @@ let numero1;
 let numero2;
 
 //asignar un valor operador de asignacion =
-apellido = "Gutierrez";//cadena de carateres o String
+apellido = "Gutierrez";//cadena de caracteres o String
 dni = 654345678; //Number 
 
 //reasignar valores
 dni= "uno, dos ,tres ,cuatro , cinco";//string
-apellido = 3.1416;
+apellido = 3.1416;//Número
 
 //constantes
 const PI =3.1416;
-//PI="Hola";NO SE PUEDE HACER
-//error lens theme => ONE DARK PRO
+//PI="Hola";NO SE PUEDE HACER PRUEBEN REASIGNAR EL VALOR A UNA CONSTANTE EN OTRA LINEA
+
 //Operaciones basicas
 
 numero1= 2;
 numero2= 145;
-let resultado = numero1 + 12;//adición
-let resta= 3.21 - 0.9;
+let resultado = numero1 + 12;//adición o suma
+let resta= 3.21 - 0.9;//Resta
+let multi= 120 * 44;//Multiplicación
+let division= 21 / 7;//División
 
-
-//Concatenacion Variables
+//Concatenación Variables
 nombre= "Daniel";
 apellido = "Perez";
 
 let nombre_completo= nombre + " " + apellido;
 
-//Métodos de salida console.log() ARMA SCRETA DEL PROGRAMADOR
+//Métodos de salida console.log() ARMA SECRETA DEL PROGRAMADOR
 
 console.log("Hola");
 console.log(2 * 3);
@@ -46,7 +49,7 @@ console.log(resultado);
 console.log(resta);
 console.log("El nombre del alumno es " + nombre_completo);
 
-//Metodo de entrada
+//Método de entrada
 
 let ingresoDato= prompt("Ingresa tu edad")
 
@@ -60,5 +63,5 @@ numero1= parseFloat(numero1);
 
 resultado = numero1 + numero2;
 
- console.log(resultado);
+console.log(resultado);
 console.log(12+"3");
